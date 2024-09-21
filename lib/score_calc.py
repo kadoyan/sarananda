590ef81ec598ba2256f415e8cd27bf47d0f4d82f
KAZUAKI KADOYA <kadoya@penpale.jp>
2024-09-21 17:21:41 +0900

    スコア計算方法修正

--- lib/score_calc.py
+++ lib/score_calc.py
@@ -8,29 +8,33 @@ class ScoreCalc:
         max_shield = scene_manager.max_shield # シールド最大値
         damage = scene_manager.damages # ダメージ回数
         energy = scene_manager.energies # 取得アイテム
-        
-        base_time = distance * 1.5 #標準クリアタイム
 
-        # タイムスコア
-        distance_score = int((position / distance) * (base_time / time)) * 1000 - damage * 10
-        print((position / distance) * (base_time / time), damage)
+        # 基本スコア(進んだ距離 ÷ ステージ全体の距離) × (進んだ距離 ÷ クリア時間)
+        progress = min(position / distance, 1)
+        distance_score = int(progress * (position / time) * distance)
+        # クリアボーナス
+        comp_bonus = 30000 if progress >= 1 else 0
         # シールド残量スコア
-        shield_score = shield * 10
+        shield_score = shield * 300
         # シールド最大ボーナス
         fullshield_score = 0
         if shield >= max_shield:
-            fullshield_score = 5000
+            fullshield_score = 10000
         # 取得エナジーボーナス
-        energy_score = energy * 100
+        energy_score = energy * 500
         # ノーダメージボーナス
         nodamage_score = 0
         if damage <= 0:
             nodamage_score = 50000
+        # ダメージペナルティ
+        damage_penalty = damage * -100
         
         return {
             "distance_score": distance_score,
+            "comp_bonus": comp_bonus,
+            "damege_penalty": damage_penalty,
             "shield_score": shield_score,
             "fullshield_score": fullshield_score,
             "energy_score": energy_score,
-            "nodamage_score": nodamage_score,
+            "nodamage_score": nodamage_score
         }
